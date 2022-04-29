ea85a216e25d9664069ea41e600b9b8f18b3351f
mochbili <mochbili@gmail.com>
2022-04-29 22:14:29 +0700

    experiment with middleware

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use(function (req, res, next) {
+// 	console.log('first piece of midddleware');
+// 	req.myMessage = 'custom message from first middleware';
+// 	next();
+// })
+
+// app.use('/different/:id', function (req, res, next) {
+// 	console.log('second piece of midddleware, ID:', req.params.id);
+// 	console.log(req.myMessage);
+
+// 	// mengambil value dari query localhost:3000/different?color=red
+// 	console.log('the colour is', req.query.color);
+// 	next();
+// })
+
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 });
